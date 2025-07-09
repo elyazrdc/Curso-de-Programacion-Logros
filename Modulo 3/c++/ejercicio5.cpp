9722cc2a3ea14254aa77a5a153331274bc050917
elyazrdc <eliasdavid200412@gmail.com>
2025-07-09 15:47:18 -0400

    ejercicio 5

--- Modulo 3/c++/ejercicio5.cpp
+++ Modulo 3/c++/ejercicio5.cpp
@@ -1,1 +1,28 @@
 //Crea un programa que simule un juego de adivinanza. El programa debe generar un número secreto (por ejemplo, int numeroSecreto = 42;). Luego, debe pedir al usuario que adivine el número. Utiliza un bucle while para que el programa siga pidiendo un número mientras el usuario no adivine el correcto. Proporciona pistas como "más alto" o "más bajo".
+
+#include<iostream>
+
+int main() {
+    int numeroSecreto = 4;
+    int numeroAdi;
+
+    do {
+        std::cout << "Dime un numero, adivinaremos el numero secreto\n";
+        std::cin >> numeroAdi;
+
+        if (numeroSecreto == numeroAdi) {
+            std::cout << "ADIVINASTE\n";
+        }
+        else if (numeroAdi > numeroSecreto) {
+            std::cout << "Cerca, es un numero mas bajo\n";
+        }
+        else if (numeroAdi < numeroSecreto) {
+            std::cout << "Cerca, es un numero mas alto\n";
+        }
+        else {
+            std::cout << "No es ese\n";
+        }
+    } while(numeroSecreto != numeroAdi);
+
+    return 0;
+}
