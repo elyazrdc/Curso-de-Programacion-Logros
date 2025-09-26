b4dc238a4d73e646b312464af398cf8c8f9ca661
elyazrdc <eliasdavid200412@gmail.com>
2025-09-26 10:48:18 -0400

    actualizacion portfolio

--- Modulo 2/Portafolio/app.js
+++ Modulo 2/Portafolio/app.js
@@ -5,6 +5,53 @@ const delayAfterType = 3000;
 const delayAfterErase = 500;
 const eraseSpeed = 50;
 
+// BOTONES DEL ASIDE
+
+asideItem = document.querySelectorAll('.aside-item');
+
+// btnAbout = document.getElementById('about-link');
+// btnHome = document.getElementById('home-link')
+// btnService = document.getElementById('service-link');
+// btnPortfolio = document.getElementById('portfolio-link');
+// btnContact = document.getElementById('contact-link');
+
+sectionArray = document.querySelectorAll('.section');
+
+// sectionAbout = document.getElementById('about-section');
+// sectionHome = document.getElementById('home-section');
+// sectionService = document.getElementById('service-section');
+// sectionPortfolio = document.getElementById('portfolio-section');
+// sectionContact = document.getElementById('contact-section');
+// sectionArray = []
+// sectionArray.push(sectionHome, sectionAbout,sectionService,sectionPortfolio,sectionContact);
+// console.log(sectionArray);
+
+asideItem.forEach((element) => {
+    element.addEventListener("click", () => {
+        clickedItem = element.id;
+        targetSectionId = clickedItem.replace('-link', '-section');
+        const targetSection = document.getElementById(targetSectionId);
+
+        asideItem.forEach(item => {
+            item.classList.remove('active');
+        });
+        element.classList.add('active');
+
+        sectionArray.forEach(section => {
+            section.classList.add('hidden');
+            setTimeout(() => {
+                section.classList.add('hidden-complete');
+            }, 900);
+        });
+
+        targetSection.classList.remove('hidden');
+
+        setTimeout(() => {
+            targetSection.classList.remove('hidden-complete');
+        }, 900);
+    })
+});
+
 function typeWriter() {
     if (charIndex < fullText.length) {
         // Añade el siguiente carácter al contenido del párrafo
